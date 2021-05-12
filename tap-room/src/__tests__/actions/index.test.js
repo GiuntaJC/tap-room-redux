77fc05b86226337c832e7c523be568f9c787a705
Joseph Giunta <jgnohack@gmail.com>
2021-05-11 23:36:05 -0700

    began adding the ability to delete kegs starting with an action creator

---
+++ tap-room/src/__tests__/actions/index.test.js
@@ -0,0 +1,10 @@
+import * as actions from './../../actions';
+
+describe('help list actions', () => {
+  it('deleteKeg should create DELETE_KEG action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: 'DELETE_KEG',
+      id: 1
+    });
+  });
+});
