96542772003454656744944d25098edebf548e33
Joseph Giunta <jgnohack@gmail.com>
2021-05-11 18:17:06 -0700

    add keg list reducer

--- tap-room/src/reducers/keg-list-reducer.js
+++ tap-room/src/reducers/keg-list-reducer.js
@@ -1,4 +1,18 @@
 export default (state = {}, action) => {
   const { name, brand, quantityLeft, pricePerPint, proof, id } = action;
-  
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          quantityLeft: quantityLeft,
+          pricePerPint: pricePerPint,
+          proof: proof,
+          id: id
+        }
+      })
+    default:
+      return state;
+  }
 }
